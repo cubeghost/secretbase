ea1ce5c03fbf9191f95d389ea32345c049982f74
goosey <hello@goose.im>
2014-11-13 17:26:06 -0500

    save feature

--- save.js
+++ save.js
@@ -1,10 +1,11 @@
 var page = require('webpage').create();
 var address = system.args[1];
-page.viewportSize = {
+/*page.viewportSize = {
   width: system.args[3],
   height: system.args[4]
-};
+};*/
 page.open(address , function () {
-    page.render(system.args[2]);
+    //page.render(system.args[2]);
+    page.render('img.png');
     phantom.exit();
 });
