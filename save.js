3ec9eaed5f0b037d6b89624e136a4102c581f4c1
goosey <hello@goose.im>
2014-11-13 15:57:55 -0500

    save feature

--- save.js
+++ save.js
@@ -1,10 +1,10 @@
 var page = require('webpage').create();
 var address = system.args[1];
 page.viewportSize = {
-  width: system.args[2],
-  height: system.args[3]
+  width: system.args[3],
+  height: system.args[4]
 };
 page.open(address , function () {
-    page.render('base.png');
+    page.render(system.args[2]);
     phantom.exit();
 });
--- save.php
+++ save.php
@@ -2,16 +2,23 @@
 
 if(isset($_POST['html'])) {
     $html = $_POST['html'];
-    $write = file_put_contents('file.html', $html);
-    if($write === false) {
-        die('There was an error writing this file');
-    }
-    else {
-        echo "$write bytes written to file";
-    }
-}
-else {
-   die('no post data to process');
+    $width = $_POST['width'];
+    $height = $_POST['height'];
+    
+    $tempname = md5(rand());
+    chmod("tmp/", 0777);
+    $tempHTML = 'tmp/' . $tempname . '.html';
+    $tempPNG = 'tmp/' . $tempname . '.png';
+    file_put_contents($tempHTML,$html);
+
+
+    exec('phantomjs save.js $tempHTML $tempPNG $width $height',$output,$error);
+    
+    
+    header('Content-type: image/png');
+    header('Content-Disposition: attachment; filename="base.png"');
+    echo file_get_contents($tempPNG);
+    
 }
 
 ?>
