450c58f66e107016a28d7083965983466e6e8732
goosey <hello@goose.im>
2014-11-13 17:40:36 -0500

    save feature

--- save.js
+++ save.js
@@ -1,10 +1,18 @@
 var system = require('system');
 var page = require('webpage').create();
-var address = 'file.html';
+var address = 'http://secret-base.herokuapp.com/file.html';
 /*page.viewportSize = {
   width: system.args[3],
   height: system.args[4]
 };*/
+page.onLoadFinished = function(status) {
+  var url = page.url;
+  console.log("Status:  " + status);
+  console.log("Loaded:  " + url);
+  page.render("google.png");
+  phantom.exit();
+};
+
 page.open(address , function () {
     //page.render(system.args[2]);
     page.render('img.png');
