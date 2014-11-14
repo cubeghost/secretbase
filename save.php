0899daccd48bfca5840619ffb51b49686d90b22e
goosey <hello@goose.im>
2014-11-14 15:50:05 -0500

    fixes

--- save.php
+++ save.php
@@ -11,7 +11,7 @@ if(isset($_POST['html'])) {
     //$tempPNG = 'tmp/' . $tempname . '.png';
     file_put_contents($tempHTML,$html);
 
-    $png = 'http://api.phantomjscloud.com/single/browser/v1/664746476ad374f19647c2627b4b42d6b4a72dfe/?targetUrl=http://secret-base.herokuapp.com/' . $tempHTML . '&loadImages=true&resourceUrlBlacklist=[]&viewportSize={"width":' . $width . ',"height":' . $height . ',+"zoomFactor":1.0+}&clipRectangle={+top:+0,+left:+0,+width:+' . $width . ',+height:+' . $height . '+}&requestType=png';
+    $png = 'http://api.phantomjscloud.com/single/browser/v1/664746476ad374f19647c2627b4b42d6b4a72dfe/?targetUrl=http://base.goose.im/' . $tempHTML . '&loadImages=true&resourceUrlBlacklist=[]&viewportSize={"width":' . $width . ',"height":' . $height . ',+"zoomFactor":1.0+}&clipRectangle={+top:+0,+left:+0,+width:+' . $width . ',+height:+' . $height . '+}&requestType=png';
 
 
     //exec('phantomjs --progress save.js $tempHTML $tempPNG $width $height',$output,$error);
