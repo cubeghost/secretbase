5a8406b38e7263b0df407100ba1289bff7080f2a
goosey <hello@goose.im>
2014-11-14 10:33:50 -0500

    save feature

--- save.js
+++ save.js
@@ -3,6 +3,17 @@ var page = require('webpage').create();
   width: 352,
   height: 288
 };*/
+phantom.onError = function(msg, trace) {
+  var msgStack = ['PHANTOM ERROR: ' + msg];
+  if (trace && trace.length) {
+    msgStack.push('TRACE:');
+    trace.forEach(function(t) {
+      msgStack.push(' -> ' + (t.file || t.sourceURL) + ': ' + t.line + (t.function ? ' (in function ' + t.function +')' : ''));
+    });
+  }
+  console.error(msgStack.join('\n'));
+  phantom.exit(1);
+};
 page.open('http://secret-base.herokuapp.com/file.html', function() {
     /*page.onResourceRequested = function (request) {
         console.log('Request ' + JSON.stringify(request, undefined, 4));
@@ -11,8 +22,8 @@ page.open('http://secret-base.herokuapp.com/file.html', function() {
         console.log('page opened');
         page.render('base.png',function(){
             console.log('base.png saved');
-            phantom.exit();
         });
+        phantom.exit();
         
     }, 400);
 });
