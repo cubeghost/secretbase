10738a8dff1daf17a9e2110a60f84f16f3b10a5f
goosey <hello@goose.im>
2014-11-14 09:53:17 -0500

    save feature

--- save.js
+++ save.js
@@ -1,10 +1,12 @@
-
 var page = require('webpage').create();
 page.viewportSize = {
   width: 352,
   height: 288
 };
 page.open('http://secret-base.herokuapp.com/file.html', function() {
+    page.onResourceRequested = function (request) {
+        console.log('Request ' + JSON.stringify(request, undefined, 4));
+    };
   page.render('base.png');
   phantom.exit();
 });
